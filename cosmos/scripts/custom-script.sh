d02912f10ff93b2437a072070c6997332b3fe449
Aakarshit Pandey (from Dev Box) <aakpandey@microsoft.com>
2025-06-18 12:51:30 -0700

    fix space issues

--- cosmos/scripts/custom-script.sh
+++ cosmos/scripts/custom-script.sh
@@ -144,7 +144,7 @@ if [[ $USE_ENVOY == true ]]; then
   sudo ./generate-envoy-config.sh $COSMOS_URI $ENVOY_CONNECT_TIMEOUT $ENVOY_PER_TRY_TIMEOUT 3
   sudo ./generate-envoy-cert.sh
   echo "########## Launching envoy ##########"
-  sudo envoy -c ./envoy-final.yaml --log-level debug --log-path $user_home/envoy_debug.out > $user_home/envoy_output.log 2>&1 &
+  sudo envoy -c ./envoy-final.yaml --log-level error --log-path $user_home/envoy_debug.out > $user_home/envoy_output.log 2>&1 &
   echo "########## Envoy launched ##########"
 fi
 
@@ -320,11 +320,11 @@ sudo mv /tmp/cosmos_client_logs "/tmp/$VM_NAME-system-diagnostics"
 sudo cp "$user_home/agent.out" "$user_home/agent.err" "/tmp/$VM_NAME-system-diagnostics"
 
 if [ -f "$user_home/envoy_debug.out" ]; then
-  sudo cp "$user_home/envoy_debug.out" "/tmp/$VM_NAME-system-diagnostics"
+  sudo mv "$user_home/envoy_debug.out" "/tmp/$VM_NAME-system-diagnostics"
 fi
 
 if [ -f "$user_home/envoy_output.log" ]; then
-  sudo cp "$user_home/envoy_output.log" "/tmp/$VM_NAME-system-diagnostics"
+  sudo mv "$user_home/envoy_output.log" "/tmp/$VM_NAME-system-diagnostics"
 fi
 
 if [ -f "$user_home/chaos.out" ] || [ -f "$user_home/chaos.err" ]; then
