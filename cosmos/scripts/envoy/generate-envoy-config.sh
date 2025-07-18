46010680d2061b7a4b33a3e90eb9f6688284bd7e
Aakarshit Pandey (from Dev Box) <aakpandey@microsoft.com>
2025-07-17 17:02:05 -0700

    update the config

--- cosmos/scripts/envoy/generate-envoy-config.sh
+++ cosmos/scripts/envoy/generate-envoy-config.sh
@@ -11,7 +11,7 @@ if ! command -v envoy &> /dev/null; then
     echo "Envoy installed successfully."
 fi
 
-TEMPLATE="./templates/envoy_config_hostrewrite_2.yaml"
+TEMPLATE="./templates/envoy_config_hostrewrite_strict.yaml"
 OUTPUT="envoy-final.yaml"
 
 FULL_URL="$1"
